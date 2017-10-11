6fdd34e6452eba6d2c5e2e626de45a64e8a3e637
LORDBABUINO <jorge.x7@gmail.com>
2017-10-11 12:14:43 -0300

    two-way data biding added to buy-order form

--- src/app/buy-order/buy-order.component.ts
+++ src/app/buy-order/buy-order.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuyOrderComponent implements OnInit {
 
+  public address: string = ''
+  public number: number
+  public complement: string = ''
+  public formPayment: string = ''
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  public updateAddress(address: string): void{
+    this.address = address
+  }
+
+  public updateFormNumber(number: number): void{
+    this.number = number
+  }
+
+  public updateComplement(complement: string): void{
+    this.complement = complement
+  }
+
+  public updateFormPayment(formPayment: string): void{
+    this.formPayment = formPayment
+  }
 }
