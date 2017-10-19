bee478d3572aba7fd0f107045aa672acc0d673dc
LORDBABUINO <jorge.x7@gmail.com>
2017-10-19 04:01:10 -0200

    buy-order input validation added. Bug: Number input doesn't work yet.

--- src/app/buy-order/buy-order.component.ts
+++ src/app/buy-order/buy-order.component.ts
@@ -12,6 +12,16 @@ export class BuyOrderComponent implements OnInit {
   public complement: string = ''
   public formPayment: string = ''
 
+  public validAddress: boolean = false
+  public validNumber: boolean = false
+  public validComplement: boolean
+  public validFormPayment: boolean = false
+
+  public pristineAddress: boolean = true
+  public pristineNumber: boolean = true
+  public pristineComplement: boolean = true
+  public pristineFormPayment: boolean = true
+
   constructor() { }
 
   ngOnInit() {
@@ -19,17 +29,39 @@ export class BuyOrderComponent implements OnInit {
 
   public updateAddress(address: string): void{
     this.address = address
+    this.pristineAddress = false
+
+    if(this.address.length > 3)
+      this.validAddress = true
+    else
+      this.validAddress = false
   }
 
   public updateFormNumber(number: number): void{
     this.number = number
+    this.pristineNumber = false
+
+    if(this.number >= 0)
+      this.validNumber = true
+    else
+      this.validNumber = false
   }
 
   public updateComplement(complement: string): void{
     this.complement = complement
+    this.pristineComplement = false
+
+    if(this.complement.length >= 1)
+      this.validComplement = true
   }
 
   public updateFormPayment(formPayment: string): void{
     this.formPayment = formPayment
+    this.pristineFormPayment = false
+
+    if(this.formPayment.length > 0)
+      this.validFormPayment = true
+    else
+      this.validFormPayment = false
   }
 }
