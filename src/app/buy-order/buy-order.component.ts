dcc88d6a3b4674899e473a2a0d9251a20480ef24
LORDBABUINO <jorge.x7@gmail.com>
2017-10-22 04:04:17 -0200

    Tryed to make the button responsive, but didn't work

--- src/app/buy-order/buy-order.component.ts
+++ src/app/buy-order/buy-order.component.ts
@@ -8,19 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class BuyOrderComponent implements OnInit {
 
   public address: string = ''
-  public number: number
+  public number: number = 0
   public complement: string = ''
   public formPayment: string = ''
 
   public validAddress: boolean = false
   public validNumber: boolean = false
-  public validComplement: boolean
+  public validComplement: boolean = false
   public validFormPayment: boolean = false
 
   public pristineAddress: boolean = true
   public pristineNumber: boolean = true
   public pristineComplement: boolean = true
   public pristineFormPayment: boolean = true
+
+  public formStatus: boolean = true
 
   constructor() { }
 
@@ -31,17 +33,21 @@ export class BuyOrderComponent implements OnInit {
     this.address = address
     this.pristineAddress = false
 
+    this.enableForm()
+
     if(this.address.length > 3)
       this.validAddress = true
     else
       this.validAddress = false
   }
 
-  public updateFormNumber(number: number): void{
+  public updateNumber(number: number): void{
     this.number = number
     this.pristineNumber = false
 
-    if(this.number >= 0)
+    this.enableForm()
+
+    if(this.number > 0)
       this.validNumber = true
     else
       this.validNumber = false
@@ -59,9 +65,18 @@ export class BuyOrderComponent implements OnInit {
     this.formPayment = formPayment
     this.pristineFormPayment = false
 
+    this.enableForm()
+
     if(this.formPayment.length > 0)
       this.validFormPayment = true
     else
       this.validFormPayment = false
+  }
+
+  public enableForm(): void {
+    if(this.validAddress && this.validNumber && this.validFormPayment)
+      this.formStatus = false
+    else
+      this.formStatus = true
   }
 }
