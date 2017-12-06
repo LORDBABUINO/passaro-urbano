9a4212036d62071384db3ba12f06606a4b9b7b77
LORDBABUINO <jorge.x7@gmail.com>
2017-12-06 16:14:09 -0200

    Inserting Http protocol in buy-order service

--- src/app/buy-order.service.ts
+++ src/app/buy-order.service.ts
@@ -1,6 +1,12 @@
+import { Injectable } from '@angular/core'
+import { Http } from '@angular/http'
 import { Order } from './shared/order.model'
 
+@Injectable()
 export class BuyOrderService {
+
+  constructor(private http: Http){}
+
   public makePurchase(order: Order):void{
     console.log(order)
   }
