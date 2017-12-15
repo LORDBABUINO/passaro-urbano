13f7079797b362750664eda9d1dfb1e8bbf41abd
LORDBABUINO <jorge.x7@gmail.com>
2017-12-15 07:40:27 -0200

    Buy-order reset

--- src/app/buy-order/buy-order.component.ts
+++ src/app/buy-order/buy-order.component.ts
@@ -3,99 +3,16 @@ import { BuyOrderService } from '../buy-order.service'
 import { Order } from '../shared/order.model'
 
 @Component({
-  selector: 'app-buy-order',
-  templateUrl: './buy-order.component.html',
-  styleUrls: ['./buy-order.component.css'],
+  selector: 'app-ordem-compra',
+  templateUrl: './ordem-compra.component.html',
+  styleUrls: ['./ordem-compra.component.css'],
   providers: [ BuyOrderService ]
 })
-export class BuyOrderComponent implements OnInit {
+export class OrdemCompraComponent implements OnInit {
 
-  public idBuyOrder: number
-
-  public order: Order = new Order('','','','')
-  public address: string = ''
-  public number: string = ''
-  public complement: string = ''
-  public formPayment: string = ''
-
-  public validAddress: boolean = false
-  public validNumber: boolean = false
-  public validComplement: boolean = false
-  public validFormPayment: boolean = false
-
-  public pristineAddress: boolean = true
-  public pristineNumber: boolean = true
-  public pristineComplement: boolean = true
-  public pristineFormPayment: boolean = true
-
-  public formStatus: boolean = false
-
-  constructor(private buyOrderService: BuyOrderService) { }
+  constructor(private ordemCompraService: BuyOrderService) { }
 
   ngOnInit() {
-  }
 
-  public updateAddress(address: string): void{
-    this.address = address
-    this.pristineAddress = false
-
-    if(this.address.length > 3)
-      this.validAddress = true
-    else
-      this.validAddress = false
-
-    this.enableForm()
-  }
-
-  public updateNumber(number: string): void{
-    this.number = number
-    this.pristineNumber = false
-
-    if(this.number.length >= 0)
-      this.validNumber = true
-    else
-      this.validNumber = false
-
-    this.enableForm()
-  }
-
-  public updateComplement(complement: string): void{
-    this.complement = complement
-    this.pristineComplement = false
-
-    if(this.complement.length >= 1)
-      this.validComplement = true
-  }
-
-  public updateFormPayment(formPayment: string): void{
-    this.formPayment = formPayment
-    this.pristineFormPayment = false
-
-    if(this.formPayment.length > 0)
-      this.validFormPayment = true
-    else
-      this.validFormPayment = false
-
-    this.enableForm()
-  }
-
-  public enableForm(): void {
-    if(this.validAddress && this.validNumber && this.validFormPayment){
-      this.formStatus = true
-    }
-    else {
-      this.formStatus = false
-    }
-  }
-
-  public confirmPurchase(): void{
-    this.order.address = this.address
-    this.order.number = this.number
-    this.order.complement = this.complement
-    this.order.formPayment = this.formPayment
-    this.buyOrderService.makePurchase(this.order)
-      .subscribe((idOrder: number) => {
-        this.idBuyOrder = idOrder
-      })
   }
 }
