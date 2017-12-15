5410d8d3f98119deb1c74df7d3c3d8fe5a8851ba
LORDBABUINO <jorge.x7@gmail.com>
2017-12-15 08:31:07 -0200

    ngModel included

--- src/app/buy-order/buy-order.component.ts
+++ src/app/buy-order/buy-order.component.ts
@@ -3,12 +3,12 @@ import { BuyOrderService } from '../buy-order.service'
 import { Order } from '../shared/order.model'
 
 @Component({
-  selector: 'app-ordem-compra',
-  templateUrl: './ordem-compra.component.html',
-  styleUrls: ['./ordem-compra.component.css'],
+  selector: 'app-buy-order',
+  templateUrl: './buy-order.component.html',
+  styleUrls: ['./buy-order.component.css'],
   providers: [ BuyOrderService ]
 })
-export class OrdemCompraComponent implements OnInit {
+export class BuyOrderComponent implements OnInit {
 
   constructor(private ordemCompraService: BuyOrderService) { }
 
